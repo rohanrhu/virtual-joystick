545e2f61312b8ecbead5dcb17bf6a2328938e551
Oğuzhan Eroğlu <rohanrhu2@gmail.com>
2020-10-28 02:11:52 +0300

    Commit from Windows.

--- VirtualJoystick.gd
+++ VirtualJoystick.gd
@@ -28,16 +28,16 @@ signal released
 func _ready():
     connect("mouse_entered", self, "_on_mouse_entered")
     connect("mouse_exited", self, "_on_mouse_exited")
-    
+
     container = Sprite.new()
     button = Sprite.new()
-    
+
     add_child(container)
     add_child(button)
-    
+
     container.texture = container_texture
     button.texture = button_texture
-    
+
     container.global_position = global_position
     button.global_position = container.global_position
 
@@ -47,12 +47,12 @@ func _process(delta):
 func _input(event):
     if not (event is InputEventScreenDrag) and not (event is InputEventScreenTouch):
         return
-    
+
     if current_event is InputEvent and current_event.index == event.index:
         current_event = event
     else:
         return
-    
+
     if not is_mouseover:
         if event is InputEventScreenTouch:
             if not event.pressed:
@@ -60,17 +60,17 @@ func _input(event):
                 is_trimming = false
                 button.global_position = container.global_position
                 velocity = Vector2.ZERO
-            
+
                 emit_signal("released")
-    
+
     _process_button()
 
 func _input_event(viewport, event, shape_idx):
     if not (event is InputEventScreenDrag) and not (event is InputEventScreenTouch):
         return
-    
+
     current_event = event
-    
+
     if event is InputEventScreenTouch:
         is_dragging = event.pressed
         if not is_dragging:
@@ -78,40 +78,40 @@ func _input_event(viewport, event, shape_idx):
             button.global_position = container.global_position
             velocity = Vector2.ZERO
             angle = 0.0
-            
+
             emit_signal("released")
-    
+
     _process_button()
 
 func _process_button():
     if is_dragging:
         button.global_position = current_event.position
-        
+
         var da = abs(button.global_position.x - container.global_position.x)
         var db = abs(button.global_position.y - container.global_position.y)
         var dh = sqrt(pow(da, 2) + pow(db, 2));
-        
+
         var a = button.global_position.x - container.global_position.x
         var b = button.global_position.y - container.global_position.y
         var h = sqrt(pow(a, 2) + pow(b, 2));
         angle = atan2(a, b)
-        
+
         is_trimming = dh > shape.shape.radius
-        
+
         if is_trimming:
             var x = container.global_position.x + sin(angle) * shape.shape.radius
             var y = container.global_position.y + cos(angle) * shape.shape.radius
-            
+
             button.global_position.x = x
             button.global_position.y = y
-        
+
         var v = Vector2(sin(angle), cos(angle))
         var is_changed = velocity != v
         velocity = v
-        
+
         if is_changed:
             emit_signal("controlling")
-        
+
         if is_trimming:
             emit_signal("trimming")
 
